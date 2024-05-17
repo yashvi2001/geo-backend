8935af6aefb7bd8b9530b06477d1a0ded55018e9
yashvi2001 <yamyashvi201@gmail.com>
2024-05-17 22:41:28 +0530

    get apis done

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require("../config/config.json")
 const geojsondata = require("./geojsondata");
 const user = require("./user");
 const db = {};
