572fc0bc7af0f63c016b0d0b0043392fba3d5aa4
yashvi2001 <yamyashvi201@gmail.com>
2024-05-17 22:35:55 +0530

    get apis done

--- models/index.js
+++ models/index.js
@@ -1,5 +1,3 @@
-
-
 const Sequelize = require("sequelize");
 
 const env = process.env.NODE_ENV || "development";
@@ -9,41 +7,33 @@ const user = require("./user");
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+
+sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 const establishConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
-    } catch (error) {
-        console.error("Unable to connect to the database:", error);
-    }
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 establishConnection();
-db.geojsondata = geojsondata
+db.geojsondata = geojsondata;
 db.geojsondata = geojsondata(sequelize, Sequelize.DataTypes);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 db.geojsondata.associate(db);
 
-
 db.user = user;
 db.user = user(sequelize, Sequelize.DataTypes);
 db.user.associate(db);
-
-
-
-
 
 module.exports = db;
